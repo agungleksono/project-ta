d13937e0b9c74674240827efa5ce58a4d0c81056
agungleksono <agungleksono99@gmail.com>
2022-08-02 21:39:02 +0700

    change seeder password user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,8 +9,8 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        User::create(['id' => 1, 'username' => 'admin_1', 'email' => 'admin1@gmail.com', 'password' => 'admin_1', 'status' => 1]);
-        User::create(['id' => 2, 'username' => 'customer_1', 'email' => 'customer1@gmail.com', 'password' => 'customer_1', 'status' => 2]);
-        User::create(['id' => 3, 'username' => 'trainer_1', 'email' => 'trainer1@gmail.com', 'password' => 'trainer_1', 'status' => 3]);
+        User::create(['id' => 1, 'username' => 'admin_1', 'email' => 'admin1@gmail.com', 'password' => '$2y$10$dBHYHJGxb4SAUi19dD9/quYdcloFtpN4IOmIR3VqZF0u8yposdp3y', 'status' => 1]);
+        User::create(['id' => 2, 'username' => 'customer_1', 'email' => 'customer1@gmail.com', 'password' => '$2y$10$dBHYHJGxb4SAUi19dD9/quYdcloFtpN4IOmIR3VqZF0u8yposdp3y', 'status' => 2]);
+        User::create(['id' => 3, 'username' => 'trainer_1', 'email' => 'trainer1@gmail.com', 'password' => '$2y$10$dBHYHJGxb4SAUi19dD9/quYdcloFtpN4IOmIR3VqZF0u8yposdp3y', 'status' => 3]);
     }
 }
