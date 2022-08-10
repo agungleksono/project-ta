9f85d5cd2ce38d49de03ca0847c87d6e583d270e
agungleksono <agungleksono99@gmail.com>
2022-08-10 18:21:53 +0700

    updated helper file

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -7,11 +7,19 @@ use Illuminate\Support\Facades\Storage;
 
 class Helper
 {
-	public static function deleteFileOnStorage($urlFile = null)
+	public static function deleteFileOnStorage($urlFiles = null)
 	{
 		// Get storage path to delete the image in storage
-		$prefixPath = request()->getHttpHost() . '/storage/';
-		$storagePath = Str::remove($prefixPath, $urlFile);
-		Storage::delete($storagePath);
+		// $prefixPath = request()->getHttpHost() . '/storage/';
+		$prefixPath = asset('uploads');
+		if (is_array($urlFiles)) {
+			foreach ($urlFiles as $urlFile) {
+				$storagePath = Str::remove($prefixPath, $urlFile);
+				Storage::delete($storagePath);
+			}
+		} else {
+			$storagePath = Str::remove($prefixPath, $urlFiles);
+			Storage::delete($storagePath);
+		}
 	}
 }
