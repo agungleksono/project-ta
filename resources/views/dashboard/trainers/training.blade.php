e5c4fc4aa1c0f1a69cb984f070474c234bf621fe
agungleksono <agungleksono99@gmail.com>
2022-08-26 00:02:11 +0700

    minor bug

--- resources/views/dashboard/trainers/training.blade.php
+++ resources/views/dashboard/trainers/training.blade.php
@@ -58,7 +58,7 @@
 	<div class="modal-dialog modal-dialog-centered">
 		<div class="modal-content">
 		<div class="modal-header">
-			<h5 class="modal-title" id="uploadCvModalLabel">Upload CV</h5>
+			<h5 class="modal-title" id="uploadCvModalLabel">Upload Materi</h5>
 			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 		</div>
 		<div class="modal-body px-4 upload-cv-container">
